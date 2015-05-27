d6d8512d8f846cecac4e4f10fa4f84ccf18f1d7e
Philipp Brumm <philipp@funkensturm.de>
2015-05-27 09:53:29 +0200

    remove leftover popover component experiment

--- app/components/Application/index.jsx
+++ app/components/Application/index.jsx
@@ -6,27 +6,6 @@ import FluxComponent from 'flummox/component'
 import Titlebar from './../Titlebar';
 import Calendar from './../Calendar';
 
-class Popover extends React.Component {
-  render() {
-    if (this.props.entry) {
-      return <div className="popoverlayer" onClick={this.props.flux.getActions('entries').deselectEntry} style={{
-        position: 'fixed',
-        top: 0, bottom: 0,
-        left: 0, right: 0,
-        display: 'flex',
-        zIndex: 10
-      }}>
-        <div style={{
-          margin: 'auto',
-          backgroundColor: 'white',
-          padding: 10
-        }}>{this.props.entry.id}</div>
-      </div>;
-    } else {
-      return null;
-    };
-  }
-}
 
 @DragDropContext(HTML5Backend)
 export default class Application extends React.Component {
