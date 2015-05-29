1a0fcfd3c60e2192ef6e0f67f1b3afb1f8f0abbb
Philipp Brumm <philipp@funkensturm.de>
2015-05-29 23:39:14 +0200

    move some more static inline styles into stylesheets

--- app/components/Application/index.jsx
+++ app/components/Application/index.jsx
@@ -3,6 +3,7 @@ import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd/modules/backends/HTML5';
 import FluxComponent from 'flummox/component'
 
+import styles from './Application.scss';
 import Titlebar from './../Titlebar';
 import Calendar from './../Calendar';
 
@@ -10,12 +11,7 @@ import Calendar from './../Calendar';
 @DragDropContext(HTML5Backend)
 export default class Application extends React.Component {
   render() {
-    return <div className='application' style={{
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column'
-    }}>
+    return <div className={styles.Application}>
       <Titlebar/>
       <Calendar/>
     </div>;
